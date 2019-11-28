150a30595025c2efdf07361185b53137cd962cbb
Diana <patrudiana.georgiana@yahho.com>
2019-11-28 16:18:08 +0200

    Implemented Backstab ability for each type of hero

--- src/abilities/Backstab.java
+++ src/abilities/Backstab.java
@@ -1,28 +1,116 @@
 package abilities;
 
+import common.Constants;
 import heroes.Knight;
 import heroes.Pyromancer;
 import heroes.Rogue;
 import heroes.Wizard;
 
-public class Backstab implements Visitor {
+public class Backstab extends Abilities implements Visitor {
+    private int nr_rounds;
+    private float woods_modificator;
+    Backstab() {
+        damage = Constants.Damage_Backstab;
+        nr_rounds = 0;
+        woods_modificator = 1;
+    }
+
+    public void setDamage() {
+        damage += Constants.Extra_Damage_Backstab;
+    }
+
     @Override
     public void visit(Pyromancer p) {
-
+        float woods = woods_modificator;
+        float dmg = damage;
+        float land_bonus = land_modificator;
+        if(nr_rounds == Constants.Nr_Rounds_Backstab) {
+            if (map.Map.getInstance().getlocation(p.getRow(), p.getCol()) == Constants.Woods_type) {
+                nr_rounds = -1;
+                woods += Constants.Hit_Woods;
+            }
+        }
+        nr_rounds++;
+        if (map.Map.getInstance().getlocation(p.getRow(), p.getCol()) == Constants.Woods_type) {
+            land_bonus += Constants.Woods_Bonus;
+        }
+        dmg = dmg * land_bonus;
+        dmg += Constants.Pyromancer_Modificator_B*dmg;
+        dmg = woods * dmg;
+        int result = Math.round(dmg);
+        System.out.println(result);
+        p.setHP(result);
     }
 
     @Override
     public void visit(Knight k) {
+        float woods = woods_modificator;
+        float dmg = damage;
+        float land_bonus = land_modificator;
+        if(nr_rounds == Constants.Nr_Rounds_Backstab) {
+            if (map.Map.getInstance().getlocation(k.getRow(), k.getCol()) == Constants.Woods_type) {
+                nr_rounds = -1;
+                woods += Constants.Hit_Woods;
+            }
+        }
+        nr_rounds++;
+        if (map.Map.getInstance().getlocation(k.getRow(), k.getCol()) == Constants.Woods_type) {
+            land_bonus += Constants.Woods_Bonus;
+        }
+        dmg = dmg * land_bonus;
+        dmg -= Constants.Knight_Modificator_B*dmg;
+        dmg = woods * dmg;
+        int result = Math.round(dmg);
+        System.out.println(result);
+        k.setHP(result);
 
     }
 
     @Override
     public void visit(Rogue r) {
+        float woods = woods_modificator;
+        float dmg = damage;
+        float land_bonus = land_modificator;
+        if(nr_rounds == Constants.Nr_Rounds_Backstab) {
+            if (map.Map.getInstance().getlocation(r.getRow(), r.getCol()) == Constants.Woods_type) {
+                nr_rounds = -1;
+                woods += Constants.Hit_Woods;
+            }
+        }
+        nr_rounds++;
+        if (map.Map.getInstance().getlocation(r.getRow(), r.getCol()) == Constants.Woods_type) {
+            land_bonus += Constants.Woods_Bonus;
+        }
+        dmg = dmg * land_bonus;
+        dmg += Constants.Roque_Modificator_B*dmg;
+        dmg = woods * dmg;
+        int result = Math.round(dmg);
+        System.out.println(result);
+        r.setHP(result);
 
     }
 
     @Override
     public void visit(Wizard w) {
+        float woods = woods_modificator;
+        float dmg = damage;
+        float land_bonus = land_modificator;
+        if(nr_rounds == Constants.Nr_Rounds_Backstab) {
+            if (map.Map.getInstance().getlocation(w.getRow(), w.getCol()) == Constants.Woods_type) {
+                nr_rounds = -1;
+                woods += Constants.Hit_Woods;
+            }
+        }
+        nr_rounds++;
+        if (map.Map.getInstance().getlocation(w.getRow(), w.getCol()) == Constants.Woods_type) {
+            land_bonus += Constants.Woods_Bonus;
+        }
+        dmg = dmg * land_bonus;
+        dmg += Constants.Wizard_Modificator_B*dmg;
+        dmg = woods * dmg;
+        int result = Math.round(dmg);
+        System.out.println(result);
+        w.setHP(result);
 
     }
 }
